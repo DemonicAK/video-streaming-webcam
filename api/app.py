28055a92d6efe471814ef86d7ec7a7d233db7f42
arijitkar <arijitkar29@gmail.com>
2023-04-05 21:43:15 +0530

    tryin

---
+++ api/app.py
@@ -0,0 +1,16 @@
+# declare functions and classess here
+# import statements here
+
+import index
+
+
+def main():
+    app = index.app
+    app.run()
+# main program
+
+    pass
+
+
+if __name__ == '__main__':
+    main()
